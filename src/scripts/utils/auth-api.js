41bb799352e009a5af8cd5508c2a2178c27b8d36
Aguy273 <septianyuga464@gmail.com>
2025-06-12 22:21:38 +0700

    update

--- src/scripts/utils/auth-api.js
+++ src/scripts/utils/auth-api.js
@@ -106,11 +106,7 @@ const AuthApi = {
         // Simpan URL saat ini sebagai redirect_uri setelah login
         const currentUrl = window.location.href.split("#")[0];
         localStorage.setItem("login_redirect", currentUrl);
-
-        // Tambahkan parameter untuk menangani error
-        window.location.href = `${CONFIG.API_BASE_URL}${
-            CONFIG.AUTH_API.GOOGLE_AUTH_URL
-        }?redirect_uri=${encodeURIComponent(currentUrl)}`;
+        window.location.href = `https://mycare-ai.netlify.app/?token=${token}/auth/google?redirect_uri=${encodeURIComponent(currentUrl)}`;
     },
 
     // Tambahkan metode untuk menangani callback Google
@@ -132,6 +128,8 @@ const AuthApi = {
         const userData = urlParams.get("user");
 
         if (token) {
+            localStorage.setItem("token", token);
+            localStorage.setItem("user", JSON.stringify(user));
             try {
                 const user = userData
                     ? JSON.parse(decodeURIComponent(userData))
